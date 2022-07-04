0419787f6c6db3cc55dd14dd9e77e7b3eaa9ed45
sgeidf <108654102+sgeidf@users.noreply.github.com>
2022-07-04 14:20:03 +0000

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'gje3gfh.herokuapp.com'
+      url.hostname = 'hjekshn.herokuapp.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
